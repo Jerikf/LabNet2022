fac1fffecdfb0c891e515e30736e63ddcf54e31d
Erik <jerifkc@gmail.com>
2022-06-19 23:59:53 -0300

    Avances MVC

---
+++ Lab.EF/Lab.EF.MVC2/Controllers/CategoriesController.cs
@@ -0,0 +1,34 @@
+﻿using Lab.EF.Entities;
+using Lab.EF.Logic;
+using Lab.EF.MVC2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.EF.MVC2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        CategoriesLogic categoriesLogic = new CategoriesLogic();
+        // GET: Categories
+        public ActionResult Index()
+        {
+            List<Categories> categories = categoriesLogic.GetAll();
+            List<CategoriesView> categoriesViews = categories.Select(categorie => new CategoriesView
+            {
+                CategoriesId = categorie.CategoryID,
+                CategoryName = categorie.CategoryName,
+                Description = categorie.Description,
+            }).ToList();
+            return View(categoriesViews);
+        }
+
+        [HttpPost]
+        public ActionResult Insert()
+        {
+            return View();
+        }
+    }
+}
